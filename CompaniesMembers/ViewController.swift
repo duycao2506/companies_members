e4618dea33955e0ca94752635aaf0b01a6326ae7
duycao2506 <duyckhl@gmail.com>
2019-12-04 23:04:27 +0800

    Initial Commit

---
+++ CompaniesMembers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CompaniesMembers
+//
+//  Created by Duy Cao on 12/4/19.
+//  Copyright © 2019 Duy Cao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CompaniesMembersTests/CompaniesMembersTests.swift
@@ -0,0 +1,34 @@
+//
+//  CompaniesMembersTests.swift
+//  CompaniesMembersTests
+//
+//  Created by Duy Cao on 12/4/19.
+//  Copyright © 2019 Duy Cao. All rights reserved.
+//
+
+import XCTest
+@testable import CompaniesMembers
+
+class CompaniesMembersTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
