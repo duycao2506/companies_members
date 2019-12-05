1982ac717a20433a6ecee4d39c9c2666a73b2023
duycao2506 <duyckhl@gmail.com>
2019-12-06 03:18:44 +0800

    calling api

--- CompaniesMembers/Repository/CompanyRepo.swift
+++ CompaniesMembers/Repository/CompanyRepo.swift
@@ -7,7 +7,27 @@
 //
 
 import UIKit
+import Alamofire
+import ObjectMapper
+import AlamofireObjectMapper
 
-class CompanyRepo: NSObject {
+protocol CompanyRepoProtocol {
+    func getCompanyList(success : @escaping (([Company])->()), failure : @escaping ((Error)->()))
+}
 
+class CompanyRepo: NSObject, CompanyRepoProtocol {
+    func getCompanyList(success : @escaping (([Company])->()), failure : @escaping ((Error)->())){
+        AF.request("https://next.json-generator.com/api/json/get/Vk-LhK44U").responseArray { (response: DataResponse<[Company], AFError>) in
+            if let error = response.error {
+                failure(NSError.init(domain: "", code: 1, userInfo: [NSLocalizedDescriptionKey:error.localizedDescription]))
+                return
+            }
+            do {
+                let companyList = try response.result.get()
+                success(companyList)
+            } catch let err {
+                failure(err)
+            }
+        }
+    }
 }
